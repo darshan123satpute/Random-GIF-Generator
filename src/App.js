72364f4bc3ff4527bc5be0d0d063bf29b9aeb4d7
Darshan <satputedarshan31@gmail.com>
2025-09-20 19:31:45 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,21 +1,26 @@
-import React from 'react';
-import Random from "./Random";
-import Tag from './Tag';
-const App = () => {
-  return (
-  
-    <div className='bg-slate-900 md:h-[1080px]'>
-    
-      <b className='absolute text-red-600 border border-red-600 border-[2px] bg-white mt-[40px] ml-[410px] p-4 w-[500px] text-center tracking-[15px] rounded-[20px] text-[20px]'>RANDOM GIF</b>
-      <div className='bg-red-200 absolute mt-[150px] ml-[330px] w-[660px] h-[415px] rounded-2xl border-red-500 border-t-4'>
-      <Random/>
-      </div>
-      <div className='bg-orange-100 absolute mt-[600px] ml-[330px] w-[660px] h-[458px] rounded-2xl border-yellow-400 border-t-4'>
-      <Tag/>
-      </div>
-    </div>
-  
-  )
-}
+  import React from 'react';
+  import Random from "./Random";
+  import Tag from './Tag';
 
-export default App
+  const App = () => {
+    return (
+      <div className="bg-slate-900 min-h-screen flex flex-col items-center justify-start gap-10 py-10">
+        
+        <b className="text-red-600 border-2 border-red-600 bg-white p-4 w-[90%] md:w-[500px] text-center tracking-[8px] md:tracking-[15px] rounded-2xl text-[18px] md:text-[20px]">
+          RANDOM GIF
+        </b>
+
+       
+        <div className="bg-red-200 w-[90%] md:w-[660px] min-h-[458px] rounded-2xl border-red-500 border-t-4 p-6 flex flex-col justify-between items-center">
+          <Random />
+        </div>
+
+        
+        <div className="bg-orange-100 w-[90%] md:w-[660px] min-h-[490px] rounded-2xl border-yellow-400 border-t-4 p-6 flex flex-col justify-between items-center">
+          <Tag />
+        </div>
+      </div>
+    )
+  }
+
+  export default App;
